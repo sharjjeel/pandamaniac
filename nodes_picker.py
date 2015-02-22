37fc6c1a5dd388824a88ece95b9b1f5b281513cd
Sharjeel Aziz <saziz@caltech.edu>
2015-02-22 09:16:06 -0800

    Attempt to beat highest degree picker

--- nodes_picker.py
+++ nodes_picker.py
@@ -3,12 +3,14 @@ import sys
 import json
 import networkx
 import random
+import collections
 
 filename = sys.argv[1]
 num_players, num_seeds, unique_id, j= filename.split('.')
+num_seeds = int(num_seeds)
 
-print(num_seeds)
-implemented = False
+implemented = True
+highest_degree = False
 G = networkx.Graph()
 d = json.load(open(filename))
 
@@ -16,16 +18,54 @@ for a in d:
 	for b in d[a]:
 		G.add_edge(a, b)
 
+otpt_list = []
+
 if not implemented:
-	output = 'upload.txt'
-	f = open(output, 'w')
 	nodes = G.nodes()
-	for a in range(50):
-		lst = []
-		for b in range(int(num_seeds)):
+	lst = []
+	for b in range(int(num_seeds)):
+		rc = random.choice(nodes)
+		while rc in lst:
 			rc = random.choice(nodes)
-			while rc in lst:
-				rc = random.choice(nodes)
-			lst.append(rc)
-			f.write(rc+'\n')
-	f.close()
+		lst.append(rc)
+	otpt_list = otpt_list + lst
+elif not highest_degree:
+	degrees = G.degree(G.nodes())
+	c = collections.Counter(degrees)
+	bot_highest = c.most_common(num_seeds)
+	half_of_bot = c.most_common(num_seeds/2)
+	left_seeds = num_seeds - len(half_of_bot);
+	for k, v in half_of_bot:
+		otpt_list.append(k);
+	bot_neighbors = []
+	for k in otpt_list:
+		bot_neighbors.append(G.neighbors(k))
+	intersect = set.intersection(*map(set,d))
+
+	intersect_not_top = []
+	for a in intersect:
+		if a not in bot_highest:
+			intersect_not_top.append(a)
+
+	if len(intersect_not_top) >= left_seeds:
+		i = 0
+		while left_seeds>=0:
+			otpt_list.append(intersect_not_top[i])
+			i = i+1
+			left_seeds = left_seeds - 1
+	else:
+		for i in intersect_not_top:
+			otpt_list.append(i)
+			left_seeds = left_seeds - 1
+		highest_degree_plus_left_seeds = c.most_common(num_seeds + left_seeds)
+		items = highest_degree_plus_left_seeds
+		for i in range(left_seeds):
+			otpt_list.append(items[num_seeds+ i][0])
+
+output = 'upload.txt'
+f = open(output, 'w')
+nodes = G.nodes()
+for a in range(50):
+	for b in otpt_list:
+		f.write(b+'\n')
+f.close()
